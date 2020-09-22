7a3aadab3ad17b098d52eeac07b7884e8f177188
Mark Stan <stanmarksebastian@gmail.com>
2020-09-22 19:38:33 +0300

    added navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import * as ReactBootStrap from "react-bootstrap";
+
+export default function Navbar() {
+	return (
+		<div>
+			<ReactBootStrap.Navbar bg="light" variant="light">
+				<ReactBootStrap.Navbar.Brand href="#home">
+					Goal
+				</ReactBootStrap.Navbar.Brand>
+				
+				<ReactBootStrap.Form inline className = "nav navbar-nav ml-auto">
+					<ReactBootStrap.FormControl
+						type="text"
+						placeholder="Search"
+						className="mr-sm-2 "
+					/>
+					<ReactBootStrap.Button variant="outline-primary">
+						Search
+					</ReactBootStrap.Button>
+				</ReactBootStrap.Form>
+			</ReactBootStrap.Navbar>
+		</div>
+	);
+}
