4a58355445302d97050d1d6139ad265313df4cfb
alexsincai <alex.sincai@yahoo.co.uk>
2020-09-23 18:32:29 +0300

    Fragment and importing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React, {useState} from "react";
+import React, { useState, Fragment } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import TeamList from "./components/TeamList";
 import { ThemeProvider } from "styled-components";
 
 function App() {
-	return (
-		
+    return (
+        <Fragment>
             <Navbar />
-         
-	);
+            <TeamList />
+        </Fragment>
+    );
 }
 
 export default App;
