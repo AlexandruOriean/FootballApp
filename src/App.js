7d5836b4e07ba74b9315eaeb0ddcdb6e3a877355
sorinobreja <sorinobreja93@gmail.com>
2020-09-24 19:16:44 +0300

    added about page with connections

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import CarouselHomePage from "./components/pages/CarouselHomePage";
 import TeamList from './components/TeamList';
+import About from './components/pages/About';
 
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <CarouselHomePage />
-      <TeamList/>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route
+          exact
+          path="/"
+          component = {CarouselHomePage}        
+        />
+        <Route
+          path='/about'
+          component={About}
+        />      
+          <TeamList />
+        
+      </div>
+    </Router>
   );
 }
 
