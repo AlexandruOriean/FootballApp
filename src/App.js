a455547dfb60d05a3296da9b0b634933c3147515
AlexandruOriean <alexandru.oriean@gmail.com>
2020-09-24 10:40:04 +0300

    added players component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import { ThemeProvider } from "styled-components";
+import PlayerList from "./components/PlayersList";
 
 function App() {
-	return (
-		
-            <Navbar />
-         
-	);
+  return (
+    <div>
+      <Navbar />;
+      <PlayerList />;
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/PlayersList.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function PlayersList() {
+    state = {
+        players: [],
+    };
+    
+    const [players, setPlayers] = useState([]);
+
+
+    useEffect(() => {
+        const config = {
+            method: "get",
+            url: "http://api.football-data.org/v2/competitions/2019",
+            headers: {
+                "Content-Type": "application/json",
+                "X-Auth-Token": "00790e41e6fc4447b2e54f2afd7d82dd",
+            },
+        };
+      
+
+        axios(config)
+            .then((response) => {
+                console.log(JSON.stringify(response.data));
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+      
+        return <div>{player}</div>;
+
+      
+    }
+    
+}
+
+export default PlayersList;
